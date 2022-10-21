ee2df21b35ec883a72773230747268b78cb5fa82
dunoose <leonardogallardo1@yahoo.com>
2022-10-20 17:04:10 -0700

    Week 5 python script

--- python/week5lab.py
+++ python/week5lab.py
@@ -1,7 +1,7 @@
 from selectors import EpollSelector
 
-
-testPassword = "goodPassword007!"
+#Variables
+testPassword = input("Choose a Password.")
 upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 upperCaseTest = False
 lowercase = "acbdefghijklmnopqrstuvwxyz"
@@ -12,10 +12,14 @@ testNumber = False
 special = "!@#$%^*()_<>?"
 specialTest = False
 
+#Lets user know what it is testing for.
 print("Testing for:", testPassword)
 
+#Tests for Password Length must be greater than 8 Characters
 if 8 <= len(testPassword):
     print("Your password PASSED the length Test")
+
+#Tests for Capital Characters
 for a in testPassword:
     if a in upperCase:
         upperCaseTest = True
@@ -25,6 +29,7 @@ for a in testPassword:
     else:
         print("Your Password FAILED the Uppercase Test")
 
+#Tests for lowercase characters
 for i in testPassword:
     if i in lowercase:
         lowerCaseTest = True
@@ -34,6 +39,17 @@ for i in testPassword:
     else:
         print("Your Password FAILED the Special Test")
 
+#Tests for Special Characters.
+for c in testPassword:
+    if c in special:
+        specialTest = True
+    if specialTest == True:
+        print("Your Password PASSED the Special Test")
+        break
+    else:
+        print("Your Password FAILED the Special Test")
+
+#Tests for intergers
 for n in testPassword:
     if n in number:
         testNumber = True
@@ -42,3 +58,9 @@ for n in testPassword:
         break
     else:
         print("Your Password FAILED the Number Test")
+
+#Lets the user know if the password met all the conditions or not.
+if upperCaseTest == True and lowerCaseTest == True and specialTest == True and testNumber == True:
+    print("Your password passed all the tests.")
+else:
+    print("Your password did not match all the conditions.")
